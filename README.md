d2a1c1b898407a4d64466ed3542df60876fbb0ad
oyakymchuk <oleksii.yakymchuk@gmail.com>
2024-09-26 18:56:43 +0300

    Added .gitignore, added config file with UPL SofaScode configuration data, started TODO list in README.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # upl_shotmap
-Scrapping SofaScore for UPL matches shots. Visualising shotmap in different sections.
+Scrapping SofaScore for UPL matches shots information. Visualising shotmap in different sections.
+
+## TODO:
+1. Create extraction of all UPL matches ids by a season.
---
+++ config.py
@@ -0,0 +1,13 @@
+SOFASCORE_API = "https://api.sofascore.com/api/v1/"
+
+# manually searched on SofaScore
+UPL_UNIQUE_TORNUMENT_ID = 218
+
+# manually searched on SofaScore
+UPL_SEASONS_ID = {
+    '24/25': 62656,
+    '23/24': 52774,
+    '22/23': 45034,
+    '21/22': 37040,
+    '20/21': 29387
+}
